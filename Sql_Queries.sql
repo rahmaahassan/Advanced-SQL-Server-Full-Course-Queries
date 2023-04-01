e7568321b511cc630f5e4f30828c0a699f745f65
Rahma Hassan <61939810+rahmaahassan@users.noreply.github.com>
2023-04-01 22:52:59 +0200

    Update Sql_Queries.sql

--- Sql_Queries.sql
+++ Sql_Queries.sql
@@ -207,3 +207,260 @@ delete from Employee where ssn = 223344
 
 -- Try to update all salaries of employees who work in Project "Al Rabwah" by 30%
 update Employee set salary = salary + salary * 0.3
+
+
+'''Use AdventureWorks DB'''
+
+-- Display the SalesOrderID, ShipDate of the SalesOrderHeader table (Sales schema) to show SalesOrders that occurred within the period ‘7/28/2002’ and ‘7/29/2014’
+Select SalesOrderID, ShipDate
+from sales.SalesOrderHeader
+where OrderDate between '07-29-2002' and '07-28-2014'
+
+-- Display only Products(Production schema) with a StandardCost below $110.00 (show ProductID, Name only)
+select ProductID, Name
+from Production.Product
+where StandardCost < 110.00
+
+-- Display ProductID, Name if its weight is unknown
+select ProductID, Name
+from Production.Product
+where Weight is NULL
+
+-- Display all Products with a Silver, Black, or Red Color
+select ProductID, Name
+from Production.Product
+where Color in ('red', 'silver', 'black')
+
+-- Display any Product with a Name starting with the letter B
+select ProductID, Name
+from Production.Product
+where name like 'B%'
+
+--Run the following Query
+--UPDATE Production.ProductDescription
+--SET Description = Chromoly steel_High of defects
+--WHERE ProductDescriptionID = 3
+--Then write a query that displays any Product description with underscore value in its description
+update Production.ProductDescription
+set Description = 'Chromoly steel_high of defects'
+where ProductDescriptionID = 3
+
+select Description
+from Production.ProductDescription
+where Description like '%[_]%'
+
+-- Calculate sum of TotalDue for each OrderDate in Sales.SalesOrderHeader table for the period between  '7/1/2001' and '7/31/2014'
+select sum(TotalDue) TotalDue
+from Sales.SalesOrderHeader
+where OrderDate between '07-01-2001' and '07-031-2014'
+
+-- Display the Employees HireDate (note no repeated values are allowed)
+select distinct HireDate
+from HumanResources.Employee
+
+-- Calculate the average of the unique ListPrices in the Product table
+select avg(distinct ListPrice) Average
+from Production.Product
+
+-- Display the Product Name and its ListPrice within the values of 100 and 120 the list should has the following format "The [product name] is only! [List price]" (the list will be sorted according to its ListPrice value)
+select Name, ListPrice
+from Production.Product
+where ListPrice between 100 and 200
+order by ListPrice
+
+-- a)	 Transfer the rowguid ,Name, SalesPersonID, Demographics from Sales.Store table  in a newly created table named [store_Archive]
+--Note: Check your database to see the new table and how many rows in it?
+--b)	Try the previous query but without transferring the data? 
+select rowguid, Name, SalesPersonID, Demographics into sales.Store_Archive
+from Sales.Store
+
+select rowguid, Name, SalesPersonID, Demographics 
+from Sales.Store_Archive
+
+select rowguid, Name, SalesPersonID, Demographics 
+from Sales.Store
+
+-- Using union statement, retrieve the today’s date in different styles using convert or format funtion.
+select convert(varchar(20), GETDATE(), 1) as [MM/DD/YY]
+union all
+select convert(varchar(20), GETDATE(), 2) as [MM.DD.YY]
+union all
+select convert(varchar(50), GETDATE(), 3) as [MM DD YY HH:MM:SS]
+
+
+'''Use ITI DB'''
+
+--	Create a scalar function that takes date and returns Month name of that date.
+create function getmname(@date date)
+returns varchar(10)
+as
+begin
+    declare @mname varchar(10)
+    select @mname = format(@date , 'MMMM')
+	return @mname
+end 
+
+select dbo.getmname('2019-02-01')
+
+-- Create a multi-statements table-valued function that takes 2 integers and returns the values between them.
+create function getnum(@num1 int, @num2 int)
+returns @temp table 
+              (nom int)
+as
+begin
+    while @num1 < @num2
+	begin
+	    set @num1 += 1
+		if @num1 = @num2
+		   break
+		insert into @temp values(@num1)
+	end
+	return
+end
+
+--- drop function getnum
+
+select* from getnum(2,7)
+
+
+-- Create inline function that takes Student No and returns Department Name with Student full name.
+create function getstd(@stno int)
+returns table     
+as
+return (
+     select s.st_fname + ' ' + s.st_lname full_name, d.dept_name
+	 from Student s join department d
+	 on d.dept_id = s.dept_id
+	 where s.St_Id = @stno
+	 )
+
+select* from getstd(10)
+
+-- Create a scalar function that takes Student ID and returns a message to user
+--a.If first name and Last name are null then display 'First name & last name are null'
+--b.If First name is null then display 'first name is null'
+--c.If Last name is null then display 'last name is null'
+--d.Else display 'First name & last name are not null'
+create function getstdname(@sid int)
+returns varchar(50)
+begin
+declare @msg varchar(50)
+     select @msg = case 
+	          when st_fname is null and st_lname is null then 'first name and last name are null'
+			  when st_fname is null then 'first name is null'
+			  when st_lname is null then 'last name is null'
+			  else 'first name and last name are not null' end 
+	from Student 
+	where @sid = St_Id
+	return @msg
+end
+
+select dbo.getstdname(13)
+
+-- Create inline function that takes integer which represents manager ID and displays department name, Manager Name and hiring date 
+create function getmng(@mid int)
+returns table 
+as
+return (
+        select d.dept_name, i.ins_name, d.manager_hiredate
+		from department d join instructor i
+		on d.dept_id = i.dept_id
+		where @mid = d.dept_manager and d.dept_manager = i.ins_id
+)
+
+--- drop function getmng
+
+select * from dbo.getmng(4)
+
+--Create multi-statements table-valued function that takes a string
+--If string='first name' returns student first name
+--If string='last name' returns student last name 
+--If string='full name' returns Full Name from student table 
+--Note: Use “ISNULL” function
+create function getstdinfo(@str varchar(20))
+returns @table table(
+                 msg varchar(20))
+as
+begin
+     if @str = 'first name'
+	 insert into @table
+	 select ISNULL(St_Fname, ' ' )
+	 from Student
+	 if @str = 'last name'
+	 insert into @table
+	 select ISNULL(st_lname, ' ')
+	 from Student
+	 if @str = 'full name'
+	 insert into @table
+	 select ISNULL(st_fname, ' ') + ' ' + ISNULL(st_lname, ' ') [full name]
+	 from Student
+return
+end
+
+select * from getstdinfo('full name')
+
+-- Write a query that returns the Student No and Student first name without the last char
+select St_Id, SUBSTRING(St_Fname, 1, len(st_fname) -1)
+from Student
+
+-- Wirte query to delete all grades for the students Located in SD Department 
+delete from Stud_Course
+where St_Id in (
+            	select s.St_Id
+				from Department d join Student s
+				on d.Dept_Id = s.Dept_Id
+				where d.Dept_Name = 'SD'
+				)
+
+
+-- Give an example for hierarchyid Data type
+create table earth_g
+(
+ node hierarchyid,
+ g_name varchar(20),
+ g_type varchar(30)
+ )
+
+insert earth_g
+values
+('/1/', 'asia', 'continent'),
+('/2/', 'africa', 'continent'),
+('/3/', 'oceania', 'continent'),
+
+('/1/1/', 'china', 'country'),
+('/1/2/', 'japan', 'country'),
+('/1/3/', 'south korea', 'country'),
+('/2/1/', 'south africa', 'country'),
+('/2/2/', 'egypt', 'country'),
+('/3/1/', 'australia', 'country'),
+
+('/', 'earth', 'planet')
+
+select node, Node.GetLevel(), g_name, g_type
+from earth_g
+
+-- Create a batch that inserts 3000 rows in the student table(ITI database). The values of the st_id column should be unique and between 3000 and 6000. All values of the columns st_fname, st_lname, should be set to 'Jane', ' Smith' respectively.
+create table students (
+                       st_id int not null,
+					   st_fname varchar(15), 
+					   st_lname varchar(15))
+
+go
+set showplan_text off
+go
+declare @id int = (select st_id from students)
+declare @st_fname varchar(15) = (select st_fname from students)
+declare @st_lname varchar(15) = (select st_lname from students)
+
+set @id = 1
+set @st_fname = 'Jane'
+set @st_lname = 'Smith'
+
+while @id <= 3000
+begin
+    insert into Students 
+	values(@id, @st_fname, @st_lname)
+	set @id +=1
+end
+
+select * from students
